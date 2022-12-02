84d531717987a83b171c868bc5b937a6b59db7d8
bekkkks <ewefe811@gmail.com>
2022-12-02 20:22:13 +0600

    homeWork1

---
+++ lesson1.py
@@ -0,0 +1,12 @@
+class Human:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+class Run:
+    def run(name):
+        print(name,'is runn')
+
+
+hum=Human('Sadir',36)
+Run.run(hum.name)
+
