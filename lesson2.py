f746ef6fc4fc50fb427d79cd59482ac65bf72335
bekkkks <ewefe811@gmail.com>
2022-12-06 20:13:54 +0600

    create lesson2

---
+++ lesson2.py
@@ -0,0 +1,51 @@
+# Объектно ориентированное программирование
+# наследование полиморфизм
+# git
+
+# супер класс == главный класс
+class Human:
+    head=1
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def __str__(self):
+        return f'name is {self.name}\n' \
+               f'age is {self.age}\n'
+
+    def run(self):
+        print(f'{self.name} is running')
+
+hum=Human('Sadir',36)
+print(hum,hum.head)
+# hum.run()
+
+# дочерний класс
+class Student(Human):
+    head = 2
+    def __init__(self, name, age,lastname):
+        super().__init__(name, age)
+        Human.__init__(self,name,age)
+        self.lastname=lastname
+    def fly(self):
+        print(self.name, 'is fly in True')
+
+    def __str__(self):
+        return f'name is {self.name}\n' \
+               f'age is {self.age}\n' \
+               f'lastname is {self.lastname}\n'
+
+student=Student('Daniyel',60,'Ermekov')
+
+print(student,student.head)
+# student.run()
+student.fly()
+
+# дочерний класс
+class Teacher(Student):
+    def fly(self):
+        print(f'{self.name} is flying')
+
+tech=Teacher('Beka',55,'DJU')
+tech.fly()
+# tech.run()
+
--- main.py
+++ main.py
@@ -1,49 +1,11 @@
-# def printe(name):
-#     print(f'{name} its may name')
-#
-# printe('Beka')
-# printe('Bakyt')
-
-class Human:
-    # атрибут уровня класса
-    head=1
-    hand=2
-    # конструктор класса,метод класса,функция
+class H:
+    atribyt=1
     def __init__(self,name,age):
         self.name=name
         self.age=age
-    # функция внутри класса называется метод
-    def printt(self):
-        print(self.name, 'is runn')
+    def run(self):
+        print('run')
 
-beka=Human('beka',19)
-beka.printt()
-hum2=Human('Nurbek',20)
-print(hum2.printt(),hum2.age)
-# # обьект класса
 
-# human=Human('Beka',19)
-# print(human.name,human.age)
-# print(human.head)
-# human2=Human('jumabek',20)
-# print(human2.name,human2.age)
-# print(human2.head)
-#
-#
-#
-class Car:
-    motor=1
-    def __init__(self,model,age,mark):
-        self.model=model
-        self.age=age
-        self.mark=mark
-    def __str__(self):
-        return f'model is {self.model}\n' \
-               f'age is {self.age}\n' \
-               f'mark is {self.mark}'
-
-car1=Car('bmw',2010,'x5')
-print(car1)
-print()
-car2=Car('mersedes',2021,'S')
-print(car2)
+g=H('name',19)
+g.run()
